d5c63e0bfbf3e5e70b3a15e4bad638890dff3c0b
c <c.predebon@zeuslab.it>
2020-04-14 21:12:57 +0200

    ic

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -13,7 +13,7 @@ const About = () => {
         
         <p>I'm twenty-one and I'm a curious front end developer who loves to always keep up to date with new stuff.</p>
 
-        <p>I am currently work at <a href="https://zeuslab.it">Zeuslab</a> on the research and development team. My mainly involvement with the team is to develop and maintain web applications.</p>
+        <p>I currently work at <a href="https://zeuslab.it">Zeuslab</a> on the research and development team. My mainly involvement with the team is to develop and maintain web applications.</p>
 
         <p>Off work I love to read, play video games, go on with personal projects and taking walks.</p>
       </Navbar>
